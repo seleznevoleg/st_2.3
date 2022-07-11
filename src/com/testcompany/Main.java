a7a54b5a8c5c17054bc30151315c9a10469e0bd6
oleg seleznev <seleznev@physics.msu.ru>
2022-07-11 12:26:48 +0300

    Stepik Java for beginners course 2.3

---
+++ src/com/testcompany/Main.java
@@ -0,0 +1,11 @@
+package com.testcompany;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [] a = {1, 2, 3};
+        System.out.println(a);
+	// write your code here
+    }
+}
